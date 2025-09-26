29fbf3a7d339f28787b7eef00264ea2c7262ddba
Koen-van-Santen <kvs.koenvansanten@gmail.com>
2025-09-26 13:20:18 +0200

    feature/09-Log out button
    Added a functional log out button to the toolbar at the top right of the screen

--- Grocery.App/AppShell.xaml.cs
+++ Grocery.App/AppShell.xaml.cs
@@ -12,5 +12,11 @@ namespace Grocery.App
             Routing.RegisterRoute(nameof(ChangeColorView), typeof(ChangeColorView));
             Routing.RegisterRoute("Login", typeof(LoginView));
         }
+        private async void OnLogoutClicked(object sender, EventArgs e)
+        {
+            bool confirm = await Application.Current.MainPage.DisplayAlert("Uitloggen", "Weet u zeker dat u wilt uitloggen?", "Ja", "Nee");
+            if (confirm)
+                await Shell.Current.GoToAsync("Login");
+        }
     }
 }
